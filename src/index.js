ad1ab8c13129e46f94399d738b57ab267d09e9f3
Meghna Srivastava <meghna.s1906@gmail.com>
2018-04-26 17:25:10 +0200

    connecting store to react with connect function

--- src/index.js
+++ src/index.js
@@ -5,8 +5,14 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import reducer from './store/reducer';
+import { Provider } from 'react-redux';
 
 const store = createStore(reducer);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+    , document.getElementById('root'));
+
 registerServiceWorker();
