85aca36d17c885511c4d91c12ff957d207093f45
Meghna Srivastava <meghna.s1906@gmail.com>
2018-04-26 15:08:42 +0200

    understanding redux in an independent file

--- redux-basics.js
+++ redux-basics.js
@@ -8,18 +8,38 @@ const initialState = { counter: 0 };
 
 // Reducer
 const rootReducer = (state = initialState, action) => { //set default state
-    return state; //simplest reducer returning the old state
+    if (action.type === 'INC_COUNTER') {
+        // state.counter++; this is not the right way, since we're mutating the original state
+        // return state; 
+
+        // instead return a new js object with updated state
+        return {
+            // copy the old state with spread opearator
+            ...state,
+            // then overwrite the one property you want to adjust
+            //and if that's also a js object, first copy that too so to avoid mutating any data
+            counter: state.counter + 1
+        };
+        if (action.type === 'ADD_COUNTER') {
+            return {
+                ...state,
+                counter: state.counter + action.value
+            };
+        }
+        return state;   
+    }
 }
 
 
-// Store
-const store = createStore(rootReducer);
-console.log(store.getState()); //its a func that pulls out the state from the store
+    // Store
+    const store = createStore(rootReducer);
+    console.log(store.getState()); //its a func that pulls out the state from the store
+    // this is the first console log when store is initalized returning initial state
 
-
-// Dispatching Action
-
-
+    // Dispatching Action
+    store.dispatch({ type: 'INC_COUNTER' });
+    store.dispatch({ type: 'ADD_COUNTER', value: 10 });
+    console.log(store.getState());
 
 
 // Subscription
