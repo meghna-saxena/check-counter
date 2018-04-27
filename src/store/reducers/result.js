5541995a48edb294f08ac9d6a7c026213f70d6bd
Meghna Srivastava <meghna.s1906@gmail.com>
2018-04-27 16:57:07 +0200

    combining multiple reducers

--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -1,36 +1,15 @@
-import * as actionTypes from './actions';
+import * as actionTypes from '../actions';
 
 const initialState = {
-    counter: 0,
     results: []
-}
+};
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
-        case actionTypes.INCREMENT:
-            return {
-                ...state,
-                counter: state.counter + 1 //changing state immutably
-            }
-        case actionTypes.DECREMENT:
-            return {
-                ...state,
-                counter: state.counter - 1
-            }
-        case actionTypes.ADD:
-            return {
-                ...state,
-                counter: state.counter + action.value
-            }
-        case actionTypes.SUBTRACT:
-            return {
-                ...state,
-                counter: state.counter - action.value
-            }
         case actionTypes.STORE_RESULT:
             return {
                 ...state, //distribute the old state
-                results: state.results.concat({ id: new Date(), value: state.counter })
+                results: state.results.concat({ id: new Date(), value: action.result })
 
                 // concat() method returns a new array so we are indeed updating immutably there
                 //id corresponds to new date, i.e when is the item added
@@ -52,3 +31,4 @@ const reducer = (state = initialState, action) => {
 };
 
 export default reducer;
+
