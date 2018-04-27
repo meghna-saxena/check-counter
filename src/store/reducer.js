48d61389878747c1f07a407fa533bdf0095ac409
Meghna Srivastava <meghna.s1906@gmail.com>
2018-04-27 12:44:01 +0200

    updating state immutably

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,24 +1,34 @@
 const initialState = {
-    counter: 0
+    counter: 0,
+    results: []
 }
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case 'INCREMENT':
             return {
+                ...state,
                 counter: state.counter + 1 //changing state immutably
             }
         case 'DECREMENT':
             return {
+                ...state,
                 counter: state.counter - 1
             }
         case 'ADD':
             return {
+                ...state,
                 counter: state.counter + action.value
             }
         case 'SUBTRACT':
             return {
+                ...state,
                 counter: state.counter - action.value
+            }
+        case 'STORE_RESULT':
+            return {
+                ...state, //distribute the old state
+                results: state.results.concat({id: new Date(),value: state.counter}) //id corresponds to new date, i.e when is the item added
             }
     }
     return state; //default return current state 
